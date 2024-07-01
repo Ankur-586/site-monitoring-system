a559e6f2fe9c78e2e8f408ce4c0cec0cdda2a2ad
Ankur586 <ankur.singh586@gmail.com>
2024-07-01 22:35:13 +0530

    response-time-underproccess

--- monitor/views.py
+++ monitor/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from authenticate.models import CustomUser
 import requests
 from django.http import JsonResponse
+import time
 
 def home(request):
     for e in CustomUser.objects.all():
@@ -13,18 +14,36 @@ def home(request):
 def get_url_data(request):
     if request.method == 'POST':
         website_url = request.POST.get('website-url')
+        # Ensure the URL starts with 'https://'
+        if not website_url.startswith('https://'):
+            website_url = 'https://' + website_url
         try:
+            start_time = time.perf_counter()
+            
             response = requests.get(website_url)
+
+            end_time = time.perf_counter()
+
+            # Measure response time
+            response_time = response.elapsed.total_seconds()
+
+            # Measure total load time
+            load_time = end_time - start_time
+            
             return JsonResponse({
-                'status': response.status_code
+                'status': response.status_code,
+                'response_time': response_time,
+                'load_time':load_time,
+                'content': response.text 
             })
-        except Exception as e:
+        except requests.exceptions.RequestException as e:
             return JsonResponse({
                 'error': str(e)  # Convert exception to string for JSON response
             })
-
-    # Handle other HTTP methods (GET, PUT, DELETE, etc.) if needed
     return JsonResponse({
         'error': 'Invalid method'  # Example error response for other methods
     })
-    
+
+# ip, ssl expiry, technolgies
+# PageSpeed Insights
+# how do you decide the load time of a website
